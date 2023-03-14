da829e0a741777a4fd8aeec2976f94ef69e07717
naengku3 <127822478+naengku3@users.noreply.github.com>
2023-03-14 22:14:53 +0900

    Update fadd.c

--- fadd.c
+++ fadd.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-	int a=2, b=3, c;
+	int a, b, c;
 	FILE* fp;
 	fp = fopen("my.txt","r");
 	fscanf(fp, "%d%d", &a, &b);
-	fclose(fp)
+	fclose(fp);
 	c= a + b;
 	printf("%d\n", c);
 }
