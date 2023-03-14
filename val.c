1ca22bc0b8022598b352b4be80116be30078f324
naengku3 <127822478+naengku3@users.noreply.github.com>
2023-03-14 22:16:35 +0900

    Update val.c

--- val.c
+++ val.c
@@ -2,8 +2,8 @@
 int main ()
 {
   int val[2], s;
-  puts("더하기 프로그램 입니다.\n두 정수를 입력하세요.");
-  scanf ("%d%d", &val[0], &val[1]);
+  val[0] = 3;
+  val[1] = 4;
   s = val[0] + val[1];
   printf ("%d+%d=%d",val[0], val[1], s);
   return 0;
